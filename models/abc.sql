6733e4a9002bd2c9468d3538a7b811ad6b7b6991
crazyswampgator@gmail.com <crazyswampgator@gmail.com>
2024-10-20 17:21:08 +0000

    committed dim_customers

---
+++ models/abc.sql
@@ -0,0 +1,5 @@
+SELECT 'JOHN' AS first_name, 'DOE' AS last_name, '1970-01-01' As birthdate
+Union ALL
+SELECT 'JANE' AS first_name, 'Smith' AS last_name, '1985-03-15' AS birthdate
+Union ALL
+Select 'Bob' AS first_name, 'Johnson' AS last_name, '1978-11-22' AS birthdate 
